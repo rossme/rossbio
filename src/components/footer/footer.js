58de354c7f1c18df7e8f0fbc64cb2de4dbbaac88
Ross Buddie <r.buddie@gmail.com>
2020-09-21 19:37:31 +0100

    Updated Jumbtron and Nav and Footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -10,9 +10,9 @@ const Footer = () => (
     <Jumbotron fluid style={{ backgroundColor: 'hotpink' }}>
         <Container style={{ padding: '0' }}>
             <div className="inner-footer" >
-                <h3 class="text-center">Footer </h3>
-                <p class="text-center">Made with a Bootstrap Jumbotron</p>
-                <p class="text-center">
+                <h3 className="text-center">Footer </h3>
+                <p className="text-center">Made with a Bootstrap Jumbotron</p>
+                <p className="text-center">
                     <FontAwesomeIcon icon={["fab", "github"]} size="2x" />
                 </p>
             </div>
