c274d9611b024341ae6eeeeebcee534c2448c00c
Ross Buddie <r.buddie@gmail.com>
2020-09-22 09:58:24 +0100

    Gatsby build

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,28 @@
  */
 
 module.exports = {
-  plugins: [`gatsby-plugin-fontawesome-css`],
+    siteMetadata: {
+    title: `Ross Buddie`,
+    description: `A simple landing page for me...`,
+    author: `gatsbyjs`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#6b37bf`,
+        theme_color: `#6b37bf`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-fontawesome-css`,
+    `gatsby-plugin-react-helmet`,
+  ]
 }
