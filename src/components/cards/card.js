1005b813be665f64c015a9796094849257f7cae4
Ross Buddie <r.buddie@gmail.com>
2020-09-22 23:21:30 +0100

    Added alt to images

--- src/components/cards/card.js
+++ src/components/cards/card.js
@@ -6,7 +6,7 @@ const Cards = () => (
     <Row className="mb-4">
         <Col sm={6}>
             <Card style={{ border: 'none' }}>
-                <Card.Img variant="top" src="https://img.favpng.com/2/11/5/color-geometry-triangle-pattern-png-favpng-t7TkucugG7c4TMHeCiC1E9528.jpg" style={{ opacity: '0.9' }} />
+                <Card.Img variant="top" src="https://img.favpng.com/2/11/5/color-geometry-triangle-pattern-png-favpng-t7TkucugG7c4TMHeCiC1E9528.jpg" alt="triangle pattern" style={{ opacity: '0.9' }} />
                 <Card.Body>
                     <Card.Title>Developers love Cards</Card.Title>
                     <Card.Text>
@@ -18,7 +18,7 @@ const Cards = () => (
         </Col>
         <Col sm={6}>
             <Card style={{ border: 'none' }}>
-                <Card.Img variant="top" src="https://res.cloudinary.com/djnfnwxgu/image/upload/v1600809986/rossb/ross_if9cte.png" style={{ opacity: '1' }} />
+                <Card.Img variant="top" src="https://res.cloudinary.com/djnfnwxgu/image/upload/v1600809986/rossb/ross_if9cte.png" alt="Ross Buddie" style={{ opacity: '1' }} />
                 <Card.Body>
                     <Card.Title>Page Transitions</Card.Title>
                     <Card.Text>
