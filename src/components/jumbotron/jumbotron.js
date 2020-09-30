616ae0fcafd779ed680dd73500c4b2db94ab4193
Ross Buddie <r.buddie@gmail.com>
2020-09-30 11:37:21 +0100

    changes to navbar padding

--- src/components/jumbotron/jumbotron.js
+++ src/components/jumbotron/jumbotron.js
@@ -6,16 +6,16 @@ import "./jumbotron.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Jumbo = () => (
-    <Jumbotron fluid>
+    <Jumbotron>
         <Container className="inner-jumbo">
-            <h3 className="text-center py-2">Built w/ React & Gatsby</h3>
+            <h3 className="text-center py-2">Built w/ Gatsby.</h3>
             <p className="text-center font-weight-light py-2">
             This is a modified jumbotron that occupies the entire horizontal space of
             its parent.
             </p>
             <div className="text-center" id="social-jumbo">
-                <a href="https://www.github.com/rossme" target="_blank" rel="noopener norefferer"><FontAwesomeIcon icon={["fab", "github-square"]} size="2x" className="mx-1"/></a>
-                <a href="https://www.linkedin.com/in/rossbuddie/" target="_blank" rel="noopener norefferer"><FontAwesomeIcon icon={["fab", "linkedin"]} size="2x" className="mx-1"/></a>
+                <a href="https://www.github.com/rossme" target="_blank" rel="norefferer"><FontAwesomeIcon icon={["fab", "github-square"]} size="2x" className="mx-1"/></a>
+                <a href="https://www.linkedin.com/in/rossbuddie/" target="_blank" rel="norefferer"><FontAwesomeIcon icon={["fab", "linkedin"]} size="2x" className="mx-1"/></a>
             </div>
 
         </Container>
