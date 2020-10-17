e13330e2c0377bd99227a6859cfd878a2db5aade
Ross Buddie <r.buddie@gmail.com>
2020-10-17 10:29:49 +0100

    CSS fixes

--- src/components/jumbotron/jumbotron.js
+++ src/components/jumbotron/jumbotron.js
@@ -8,9 +8,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 const Jumbo = () => (
     <Jumbotron className="jumbotron">
         <Container className="inner-jumbo">
-            <h3 className="text-center py-2">Built w/ Love & Gatsby.</h3>
-            <p className="text-center font-weight-light py-2">
-            </p>
+            <h3 className="text-center py-2">Ross Buddie</h3>
+            <p className="text-center py-2">Web Developer</p>
             <div className="text-center" id="social-jumbo">
                 <a href="https://www.github.com/rossme" target="_blank" rel="norefferer"><FontAwesomeIcon icon={["fab", "github-square"]} size="2x" className="mx-1"/></a>
                 <a href="https://www.linkedin.com/in/rossbuddie/" target="_blank" rel="norefferer"><FontAwesomeIcon icon={["fab", "linkedin"]} size="2x" className="mx-1"/></a>
