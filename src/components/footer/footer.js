52e58fc38ccfb3164958ebcebe3cb388373f59ea
Ross Buddie <r.buddie@gmail.com>
2020-10-17 10:06:56 +0100

    Fixed footer margin

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -7,14 +7,14 @@ import "./footer.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Footer = () => (
-    <Jumbotron fluid style={{ backgroundColor: 'hotpink'}}>
+    <Jumbotron fluid className="jumbo-footer">
         <Container style={{ padding: '0' }}>
             <div className="inner-footer">
-                <h3 className="text-center">Footer </h3>
+                <h3 className="text-center">Ross Buddie</h3>
                 <div className="text-center" id="social-footer">
-                <p className="text-center"><a href="http://www.lecocktail.xyz" target="_blank" rel="norefferer">lecocktail.xyz</a></p>
-                <p className="text-center mt-0 mb-4"><a href="https://www.deve.to/" target="_blank" rel="norefferer">deve.to</a></p>
-                    <a href="#cards" target="_blank" rel="norefferer"><FontAwesomeIcon icon={["fab", "github-square"]} size="2x" className="mx-1"/></a>
+                    <p className="text-center"> <a href="http://www.lecocktail.xyz" target="_blank" rel="norefferer">lecocktail.xyz </a> 
+                    <a href="https://www.deve.to/" target="_blank" rel="norefferer"> deve.to</a></p>
+                    <a href="https://www.github.com/rossme/" target="_blank" rel="norefferer"><FontAwesomeIcon icon={["fab", "github-square"]} size="2x" className="mx-1"/></a>
                     <a href="https://www.linkedin.com/in/rossbuddie/" target="_blank" rel="norefferer"><FontAwesomeIcon icon={["fab", "linkedin"]} size="2x" className="mx-1"/></a>
                 </div>
             </div>
